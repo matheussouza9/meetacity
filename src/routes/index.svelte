863c42594205bc8f04e561b673a0361342d302c1
Matheus Souza <matheussouzadecarvalho@gmail.com>
2022-01-24 10:14:42 -0300

    Added the youtube background player

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,34 @@
-<svelte:head>
-	<title>Meet a City</title>
-</svelte:head>
+<script>
+    import YouTube from 'svelte-youtube'
 
-<h1>Meet a City</h1>
+    // https://developers.google.com/youtube/player_parameters
+    const options = {
+        playerVars: {
+            autoplay: 1,
+            mute: 1,
+            enablejsapi: 1,
+            controls: 0
+        }
+    }
+</script>
+
+<YouTube
+    id='yt-iframe'
+    videoId='5qap5aO4i9A'
+    options={options}
+/>
+
+<style>
+    :global(#yt-iframe){
+        position: fixed;
+        top: 0;
+        left: 0;
+        bottom: 0;
+        right: 0;
+        width: 100%;
+        height: 100%;
+        z-index: -2;
+        pointer-events: 0;
+        user-select: none;
+    }
+</style>
--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,10 @@
-import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-auto'
+// const pkg = require('./package.json')
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: [preprocess({
-        "postcss": true
-    })],
-
 	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
+		adapter: adapter(),
 		target: '#svelte'
 	}
 };
